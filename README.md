a53c18a51b3ceb619681cacbe1c4dbd0793aa7bd
OlgaChernytska <olha.chernytska@gmail.com>
2021-11-15 00:43:16 +0000

    added bbox calculation, trained model and requirements

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Visual-Inspection
+
+
+Pipeline to train a model that classifies items as 'good'/'defected'.
+Trained withou any labels for defected region in the images, model is able to predict bounding box with defects for images classified as 'defected'. This is achieved by utilizing feature maps of the last convolutional layers.
--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import seaborn as sns
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import SubsetRandomSampler, DataLoader
 from torchvision import transforms, datasets, models
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.metrics import confusion_matrix, accuracy_score, balanced_accuracy_score
 
 
@@ -83,7 +84,7 @@ class CustomVGG(nn.Module):
                 weights.unsqueeze(-1)
                 .unsqueeze(-1)
                 .unsqueeze(0)
-                .repeat((10, 1, 1, 14, 14))
+                .repeat((x.size(0), 1, 1, 14, 14))
             )
             feature_maps = feature_maps.unsqueeze(1).repeat((1, 2, 1, 1, 1))
             location = torch.mul(weights, feature_maps).sum(axis=2)
@@ -140,8 +141,8 @@ def evaluate(model, dataloader, device):
         preds_probs = model(inputs)[0]
         preds_class = torch.argmax(preds_probs, dim=-1)
 
-        labels = labels.cpu().numpy()
-        preds_class = preds_class.detach().cpu().numpy()
+        labels = labels.to("cpu").numpy()
+        preds_class = preds_class.detach().to("cpu").numpy()
 
         y_true = np.concatenate((y_true, labels))
         y_pred = np.concatenate((y_pred, preds_class))
@@ -171,3 +172,105 @@ def plot_confusion_matrix(y_true, y_pred, class_names="auto"):
     plt.xlabel("Predicted labels")
     plt.title("Confusion Matrix")
     plt.show()
+    
+    
+def get_bbox_from_heatmap(heatmap, thres):
+    """Return upper left and lower right corner locations."""
+    binary_map = heatmap > thres
+
+    if binary_map.sum() == 0:
+        return 0, 0, 0, 0
+
+    x_dim = np.max(binary_map, axis=0) * np.arange(0, binary_map.shape[1])
+    x_0 = int(x_dim[x_dim > 0].min())
+    x_1 = int(x_dim.max())
+
+    y_dim = np.max(binary_map, axis=1) * np.arange(0, binary_map.shape[0])
+    y_0 = int(y_dim[y_dim > 0].min())
+    y_1 = int(y_dim.max())
+
+    return x_0, y_0, x_1, y_1
+
+
+def predict_localize(model, dataloader, device, thres, neg_class, n_samples=9):
+    model.to(device)
+    model.eval()
+
+    class_names = dataloader.dataset.classes
+    transform_to_PIL = transforms.ToPILImage()
+
+    n_cols = 3
+    n_rows = int(np.ceil(n_samples / n_cols))
+    plt.figure(figsize=[n_cols * 5, n_rows * 5])
+
+    counter = 0
+    for inputs, labels in dataloader:
+        inputs = inputs.to(device)
+        out = model(inputs)
+        probs, class_preds = torch.max(out[0], dim=-1)
+        feature_maps = out[1].to("cpu")
+
+        for img_i in range(inputs.size(0)):
+            img = transform_to_PIL(inputs[img_i])
+            class_pred = class_preds[img_i]
+            prob = probs[img_i]
+            label = labels[img_i]
+            heatmap = feature_maps[img_i][neg_class].detach().numpy()
+
+            counter += 1
+            plt.subplot(n_rows, n_cols, counter)
+            plt.imshow(img)
+            plt.axis("off")
+            plt.title(
+                "Predicted: {}, Prob: {:.3f}, True Label: {}".format(
+                    class_names[class_pred], prob, class_names[label]
+                )
+            )
+
+            if class_pred == neg_class:
+                x_0, y_0, x_1, y_1 = get_bbox_from_heatmap(heatmap, thres)
+                rectangle = Rectangle(
+                    (x_0, y_0),
+                    x_1 - x_0,
+                    y_1 - y_0,
+                    edgecolor="red",
+                    facecolor="none",
+                    lw=3,
+                )
+                plt.gca().add_patch(rectangle)
+
+                plt.imshow(heatmap, cmap="Reds", alpha=0.3)
+
+            if counter == n_samples:
+                plt.tight_layout()
+                plt.show()
+                return
+            
+            
+def get_cv_train_test_loaders(root, batch_size, img_size, n_folds=5):
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize(img_size),
+            transforms.ToTensor(),
+        ]
+    )
+    dataset = datasets.ImageFolder(root=root, transform=transform)
+
+    kf = StratifiedKFold(n_splits=n_folds)
+    kf_loader = []
+
+    for train_idx, test_idx in kf.split(np.arange(dataset.__len__()), dataset.targets):
+        train_sampler = SubsetRandomSampler(train_idx)
+        test_sampler = SubsetRandomSampler(test_idx)
+
+        train_loader = DataLoader(
+            dataset, batch_size=batch_size, sampler=train_sampler, drop_last=True
+        )
+        test_loader = DataLoader(
+            dataset, batch_size=batch_size, sampler=test_sampler, drop_last=False
+        )
+
+        kf_loader.append((train_loader, test_loader))
+
+    return kf_loader
